a56190e8f34ac4390069888123a7b4a487335564
Rhittum <rhittum1802@gmail.com>
2026-04-26 03:26:43 +0530

    code buffer in memory, no file read at runtime

--- mimo.c
+++ mimo.c
@@ -4,26 +4,31 @@
 int main(int argc,char* argv[]){
 	if(argc<2) return 1;
 	FILE *fp=fopen(argv[1],"r");
+	if(!fp) return 1;
 
-	int buf[300];
+	unsigned char buf[300];
+	char code[300];
 	memset(buf,48,300);
+	int code_len = fread(code,1,300,fp);
 
-	long jump=-1, stack[32];
-	int ptr=0, c, sp=0, mov=1;
-	while((c=fgetc(fp)) != EOF) {
+	int stack[32];
+	int ptr=0, pc=0, sp=0, mov=1;
+	while(pc<code_len) {
+		char c = code[pc++];
 		switch(c) {
 			case '>':
-				mov==1?ptr++:ptr--;
+				ptr=(ptr+mov+300)%300;
 				break;
 			case '_':
-				buf[ptr]=(mov==1?++buf[ptr]:--buf[ptr])%256;
+				buf[ptr]+=mov;
 				break;
 			case ':':
-				stack[sp++]=jump=ftell(fp);
+				stack[sp++]=pc;
 				break;
 			case '?':
-				if (buf[ptr]>48 && jump!=-1){
-					fseek(fp, stack[sp-1], SEEK_SET);
+				if (buf[ptr]>48){
+					pc=stack[sp-1];
+					continue;
 				} else if (sp>0) sp--;
 				break;
 			case '^':
--- tiny_mimo.c
+++ tiny_mimo.c
@@ -1,4 +1,8 @@
 /* Syscall numbers for x86_64 */
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 #define SYS_read 0
 #define SYS_write 1
 #define SYS_open 2
@@ -9,6 +13,12 @@ long syscall3(long num, long a1, long a2, long a3) {
     long ret;
     __asm__ volatile ("syscall" : "=a"(ret) : "a"(num), "D"(a1), "S"(a2), "d"(a3) : "rcx", "r11", "memory");
     return ret;
+}
+long get_pos(int fd) {
+	return syscall3(8, fd, 0, 1);
+}
+void set_pos(int fd, long pos) {
+	syscall3(8,fd,pos,0);
 }
 
 void _start() {
@@ -22,18 +32,23 @@ void _start() {
     int fd = syscall3(SYS_open, (long)argv[1], 0, 0); // 0 = O_RDONLY
     if (fd < 0) syscall3(SYS_exit, 1, 0, 0);
 
-    unsigned char buf = 0;
-    int ptr = 0;
+    unsigned char buf[300];
+    memset(buf, 48, 300);
+    int ptr = 0, mov = 1;
+    off_t loop_pos=-1;
     char c;
 
     while (syscall3(SYS_read, fd, (long)&c, 1) > 0) {
-        if (c == '>') ptr = (ptr + 1) % 8;
-        else if (c == '+') buf ^= (1 << (7 - ptr));
-        else if (c == '.') {
-            syscall3(SYS_write, 1, (long)&buf, 1);
-            buf = 0;
-            ptr = 0;
-        }
+        if (c == '>') mov==1?ptr++:ptr--;
+        else if (c == '+') buf[ptr]=mov==1?++buf[ptr]:--buf[ptr];
+        else if (c == ':') loop_pos = get_pos(fd);
+        else if (c == '?') {
+		if (buf[ptr]>48 && loop_pos!=-1) {
+			set_pos(fd, loop_pos);
+		}
+	}
+        else if (c == '^') mov=-mov;
+        else if (c == '.') syscall3(SYS_write, 1, (long)&buf, 1);
     }
 
     syscall3(SYS_exit, 0, 0, 0);
